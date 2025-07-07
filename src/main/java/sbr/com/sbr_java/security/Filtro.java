164cd6b854af0fbf82843a3d22b5b43fa14ebd5a
Jesith Mejia <jesithmanuel@gmail.com>
2025-07-06 23:08:10 -0500

    Correccion de errores

--- src/main/java/sbr/com/sbr_java/security/Filtro.java
+++ src/main/java/sbr/com/sbr_java/security/Filtro.java
@@ -4,7 +4,7 @@
  */
 package sbr.com.sbr_java.security;
 
-import java.io.IOException;     
+import java.io.IOException;
 import java.net.Proxy;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -20,14 +20,14 @@ import javax.servlet.http.HttpSession;
  *
  * @author rosdri
  */
-public class Filtro implements Filter{
+public class Filtro implements Filter {
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
     }
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+    /*public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest solicitud = (HttpServletRequest) request;
         HttpServletResponse respuesta = (HttpServletResponse) response;
         
@@ -55,10 +55,39 @@ public class Filtro implements Filter{
         }else{
             respuesta.sendRedirect(raizSolicitud);
         }
+    }*/
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest solicitud = (HttpServletRequest) request;
+        HttpServletResponse respuesta = (HttpServletResponse) response;
+
+        HttpSession sesion = solicitud.getSession();
+        String rutaSolicitud = solicitud.getRequestURI();
+        String raizSolicitud = solicitud.getContextPath();
+
+        respuesta.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+        respuesta.setHeader("Pragma", "no-cache");
+        respuesta.setDateHeader("Expires", 0);
+
+        // ✅ Rutas públicas permitidas sin sesión
+        boolean rutaPublica = (rutaSolicitud.equals(raizSolicitud + "/")
+                || rutaSolicitud.equals(raizSolicitud + "/login.xhtml")
+                || rutaSolicitud.contains("/terminos-condiciones/terminos_condiciones.xhtml")
+                || rutaSolicitud.contains("/unete-a-nosotros/unete_nosotros.xhtml")
+                || rutaSolicitud.contains("/mapa-navegacion/mapa-navegacion.xhtm")
+                || rutaSolicitud.startsWith(raizSolicitud + "/javax.faces.resource"));
+
+        boolean sesionValida = (sesion != null && sesion.getAttribute("usuario") != null);
+
+        if (sesionValida || rutaPublica) {
+            chain.doFilter(request, response); // Deja pasar
+        } else {
+            respuesta.sendRedirect(raizSolicitud + "/login.xhtml"); // Redirige si no hay sesión
+        }
     }
 
     @Override
     public void destroy() {
     }
-    
+
 }
